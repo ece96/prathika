e6fe11aea29420d0bcd50e2ed03f9c120a2df927
ece96 <30343336+ece96@users.noreply.github.com>
2017-08-12 16:30:05 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+ int n;
+ printf("\n enter the number");
+ scanf("%d",&n);
+ if(n/2==0)
+   printf("\n the given number is prime");
+  else
+    printf("\n the given number is not prime");
+   getch();
+   }
+   
